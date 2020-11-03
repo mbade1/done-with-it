477711bade95461bed1a3366d411dfce51dbe72c
mbade1 <mbade314@gmail.com>
2020-11-03 17:35:55 -0600

    Notes updated with new information about React Native basics

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 //View -> UIView or Android View. React-native does this FOR us.
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>
+        <TouchableHighlight onPress={() => console.log("Image tapped")}>
+          <View></View>
+        </TouchableHighlight>
+        Small text again
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
