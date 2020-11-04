e4d1e0e3937f3ea3cd8274c5e77dab42d4e3126e
mbade1 <mbade314@gmail.com>
2020-11-04 16:56:33 -0600

    Progress on Menu options

--- App.js
+++ App.js
@@ -7,28 +7,59 @@ import {
   View,
   Image,
   SafeAreaView,
+  Button,
+  Alert,
+  Platform,
+  Dimensions,
 } from "react-native";
+import ViewImageScreen from "./app/assets/screens/ViewImageScreen";
+import WelcomeScreen from "./app/assets/screens/WelcomeScreen";
 
 //View -> UIView or Android View. React-native does this FOR us.
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>
-        <TouchableHighlight onPress={() => console.log("Image tapped")}>
-          <View></View>
-        </TouchableHighlight>
-        Small text again
-      </Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <ViewImageScreen></ViewImageScreen>;
+
+  // return <WelcomeScreen></WelcomeScreen>;
+  // <View
+  //   style={{
+  //     backgroundColor: "#fff",
+  //     flex: 1,
+  //     flexDirection: "row",
+  //     justifyContent: "center",
+  //     alignItems: "center",
+  //   }}
+  // >
+  //   <View
+  //     style={{
+  //       backgroundColor: "dodgerblue",
+  //       flexBasis: 100,
+  //       flexShrink: 1,
+  //       height: 100,
+  //     }}
+  //   />
+  //   <View
+  //     style={{
+  //       backgroundColor: "gold",
+  //       width: 100,
+  //       height: 100,
+  //     }}
+  //   />
+  //   <View
+  //     style={{
+  //       backgroundColor: "tomato",
+  //       width: 100,
+  //       height: 100,
+  //     }}
+  //   />
+  // </View>
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
